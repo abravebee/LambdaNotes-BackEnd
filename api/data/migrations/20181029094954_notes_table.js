f97cabad830e36eba7f862d12d9968e6327110cb
Casey Baker <caseye.baker@gmail.com>
2018-10-29 10:07:02 -0700

    seed file added

--- api/data/migrations/20181029094954_notes_table.js
+++ api/data/migrations/20181029094954_notes_table.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('notes', function(tbl) {
+    tbl.increments();
+
+    tbl
+      .string('title', 128).notNullable();
+
+    tbl
+      .string('content', 3000);
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('notes');
 };
