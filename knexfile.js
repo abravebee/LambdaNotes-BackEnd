fb502b5ddfaf308e36d64c10e3c110831e3af1c9
Casey Baker <caseye.baker@gmail.com>
2018-10-30 15:19:05 -0700

    changed file structure to hopefully make heroku work

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './api/data/db.sqlite3'
+      filename: './data/db.sqlite3'
     },
     useNullAsDefault: true,
     migrations: {
